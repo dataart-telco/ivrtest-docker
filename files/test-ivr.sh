5939e8d7adc2323030ff8d20359f8595c50be872
Gennadiy Dubina <gdubina@dataart.com>
2016-03-01 15:26:31 +0200

    fix script

--- files/test-ivr.sh
+++ files/test-ivr.sh
@@ -6,4 +6,27 @@ PHONE_NUMBER=$3
 
 ulimit -n 500000
 
-sipp -sf ./tests/ivr/ivr-sipp.xml -s $PHONE_NUMBER $RESTCOMM_ADDRESS -mi $LOCAL_ADDRESS:5090 -l 200 -m 5000 -r 80 -trace_screen -trace_err -recv_timeout 5000 -t un -nr
+CALLS_MAX="$TEST_CALLS_MAX"
+CALLS_CONC="$TEST_CALLS_CONC"
+CALLS_RATE="$TEST_CALLS_RATE"
+
+if [ -z "$CALLS_MAX" ]; then
+    CALLS_MAX=5000
+fi
+
+if [ -z "$CALLS_CONC" ]; then
+    CALLS_CONC=200
+fi
+
+if [ -z "$CALLS_RATE" ]; then
+    CALLS_RATE=80
+fi
+
+
+sipp -sf ./tests/ivr/ivr-sipp.xml -s $PHONE_NUMBER $RESTCOMM_ADDRESS \
+    -mi $LOCAL_ADDRESS:5090 \
+    -l $CALLS_CONC \
+    -m $CALLS_MAX \
+    -r $CALLS_RATE \
+    -trace_screen -trace_err \
+    -recv_timeout 5000 -t un -nr
