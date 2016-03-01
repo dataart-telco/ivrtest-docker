fff426ee4016753905553e1f01ba4e5626147d1c
Gennadiy Dubina <gdubina@dataart.com>
2016-03-01 09:38:07 +0200

    update test

--- files/test-ivr.sh
+++ files/test-ivr.sh
@@ -6,4 +6,4 @@ PHONE_NUMBER=$3
 
 ulimit -n 500000
 
-sipp -sf ./tests/ivr/ivr-sipp.xml -s $PHONE_NUMBER $RESTCOMM_ADDRESS -mi $LOCAL_ADDRESS:5090 -l 200 -m 1000 -r 10 -trace_screen -trace_err -recv_timeout 5000 -t un -nr
+sipp -sf ./tests/ivr/ivr-sipp.xml -s $PHONE_NUMBER $RESTCOMM_ADDRESS -mi $LOCAL_ADDRESS:5090 -l 200 -m 5000 -r 80 -trace_screen -trace_err -recv_timeout 5000 -t un -nr
--- run_restcomm.sh
+++ run_restcomm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ../utils/read-network-props.sh "eth0"
+source ./files/utils/read-network-props.sh "eth0"
 
 docker rm -f restcomm
 
@@ -9,4 +9,4 @@ docker run --net host -d --name=restcomm \
     -e INIT_PASSWORD=42d8aa7cde9c78c4757862d84620c335 \
     -e VOICERSS_KYE=29b2d893df9f454abbfae94df6cff95b \
     -e PROD_MODE=true \
-    hamsterksu/restcomm:7.6.0.812
+    hamsterksu/restcomm:7.6.0.823
