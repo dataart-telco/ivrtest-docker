44549ef4fbfac44b122edf6e497f3a1525e69606
root <root@ip-172-31-0-6.ec2.internal>
2016-03-11 12:43:40 +0000

    improve scritps

--- files/run.sh
+++ files/run.sh
@@ -6,6 +6,28 @@ mkdir -p $PWD/log
 
 source ./utils/read-network-props.sh "eth0"
 
+if [ -z "$TEST_CALLS_MAX" ]; then
+    TEST_CALLS_MAX=5000
+fi
+
+if [ -z "$TEST_CALLS_CONC" ]; then
+    TEST_CALLS_CONC=200
+fi
+
+if [ -z "$TEST_CALLS_RATE" ]; then
+    TEST_CALLS_RATE=80
+fi
+
+echo "**********************"
+echo "** Start ivr-test with params: "
+echo "** TEST_CALLS_RATE: $TEST_CALLS_RATE"
+echo "** TEST_CALLS_CONC: $TEST_CALLS_CONC"
+echo "** TEST_CALLS_MAX: $TEST_CALLS_MAX"
+echo "*********************"
+echo ""
+
+echo "Start ivr-server"
+
 ./ivrtest-server \
     -h "$PRIVATE_IP" \
     -p "$PORT" \
@@ -20,15 +42,25 @@ source ./utils/read-network-props.sh "eth0"
     > $PWD/log/ivrtest-server.log \
     2>&1 &
 
+echo "Wait for ivr-server (3 sec)"
 sleep 3
 
 URL="http://$PRIVATE_IP:$PORT"
-curl "$URL/start"
+curl -s "$URL/start"
 
 ./test-ivr.sh $PRIVATE_IP "$RESTCOMM_HOST:5080" $PHONE_NUMBER
 
-sleep 5
+echo "Wait 7 sec before close"
+sleep 7
 
 echo "Server stat:"
-curl "$URL/"
+curl -s "$URL/"
 
+echo ""
+echo "Dump to file: result.csv"
+
+RESULT_INCOMING=`curl -s $URL/stat/incoming`
+RESULT_RECEIVED=`curl -s $URL/stat/received`
+
+echo "`date`,$TEST_CALLS_RATE,$TEST_CALLS_CONC,$TEST_CALLS_MAX,$RESULT_INCOMING,$RESULT_RECEIVED" >> /opt/ivrtest/result/result.csv
+echo "--- TEST FINISHED ---"
--- run.sh
+++ run.sh
@@ -6,32 +6,42 @@ echo "
 **   You can specify sipp params:
 **
 **   TEST_CALLS_RATE - calls rate
-**   TEST_CALLS_MAX  - count of calls
 **   TEST_CALLS_CONC - concurrent calls 
+**   TEST_CALLS_MAX  - count of calls
 **
 **************
 "
 
 mkdir -p log
+mkdir -p result
+
 rm log/ivrtest-server.log
 
 docker rm -f ivrtest
 
-source ./files/utils/read-network-props.sh "eth0"
+if [ "$#" -eq 1 ]; then
+  RESTCOMM_HOST=$1
+else
+  echo "Detecting private ip..."
+  source ./files/utils/read-network-props.sh "eth0"
+  RESTCOMM_HOST=$PRIVATE_IP
+fi
+
+echo "Restcomm host: $RESTCOMM_HOST"
 
 docker run --rm \
     --net host \
     --name ivrtest \
     -e PORT=7090 \
-    -e RESTCOMM_HOST="$PRIVATE_IP" \
+    -e RESTCOMM_HOST=$RESTCOMM_HOST \
     -e RESTCOMM_PORT=8080 \
     -e RESTCOMM_USER=ACae6e420f425248d6a26948c17a9e2acf \
     -e RESTCOMM_PSWD=42d8aa7cde9c78c4757862d84620c335 \
     -e PHONE_NUMBER=7777 \
+    -e TEST_CALLS_RATE=40 \
+    -e TEST_CALLS_CONC=200 \
+    -e TEST_CALLS_MAX=5000 \
     -v "$PWD/log":/opt/ivrtest/log \
+    -v "$PWD/result":/opt/ivrtest/result \
     ivrtest
-
-#    -e TEST_CALLS_RATE=5 \
-#    -e TEST_CALLS_MAX=100 \
-#    -e TEST_CALLS_CONC=20 \
 
