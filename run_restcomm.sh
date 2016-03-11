99d049a61f6fbff2e4dbab28f5bdde51d7a4c69b
Gennadiy Dubina <gdubina@dataart.com>
2016-03-11 08:54:02 +0200

    fix typo

--- run_restcomm.sh
+++ run_restcomm.sh
@@ -5,7 +5,7 @@ source ./files/utils/read-network-props.sh "eth0"
 docker rm -f restcomm
 
 docker run --net host -d --name=restcomm \
-    -e STATIS_ADDRESS=$PRIVATE_IP \
+    -e STATIC_ADDRESS=$PRIVATE_IP \
     -e INIT_PASSWORD=42d8aa7cde9c78c4757862d84620c335 \
     -e VOICERSS_KYE=29b2d893df9f454abbfae94df6cff95b \
     -e MEDIASERVER_LOWEST_PORT=64000 \
--- run_restcomm_mysql.sh
+++ run_restcomm_mysql.sh
@@ -18,7 +18,7 @@ docker rm -f restcomm
 #    --cpuset-cpus="0-3" \
 
 docker run --net host -d --name=restcomm \
-    -e STATIS_ADDRESS=$PRIVATE_IP \
+    -e STATIC_ADDRESS=$PRIVATE_IP \
     -e INIT_PASSWORD=42d8aa7cde9c78c4757862d84620c335 \
     -e VOICERSS_KYE=29b2d893df9f454abbfae94df6cff95b \
     -e MEDIASERVER_LOWEST_PORT=64000 \
